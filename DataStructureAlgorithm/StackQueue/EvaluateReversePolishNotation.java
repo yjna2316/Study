0483abaaa95fc4edff80ee91d9702d261b27f8ce
Yoonji Na <yjna2316@naver.com>
2017-07-12 13:39:27 +0900

    Stack, Postfix notation

--- DataStructureAlgorithm/StackQueue/EvaluateReversePolishNotation.java
+++ DataStructureAlgorithm/StackQueue/EvaluateReversePolishNotation.java
@@ -3,24 +3,33 @@
  * https://leetcode.com/problems/evaluate-reverse-polish-notation/#/description
  */
 
+/* 
+ * What if Exception in thread "main" java.lang.ArithmeticException: / by zero, 
+ * how would you handle this?
+ */
+ 
 public class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> s = new Stack<>();
-        for(String token : tokens) {
-            if (token.equals("/") || token.equals("+") || token.equals("-") || token.equals("*")) {
-                calculate(token, s);
-            } else {
-              s.push(Integer.parseInt(token));
-            }
+        Stack<Integer> S = new Stack<>();
+        for (String token : tokens) {
+            switch (token) {
+                case "+" : 
+                    S.push(S.pop() + S.pop()); 
+                    break;
+                case "-" : 
+                    S.push(-S.pop() + S.pop()); 
+                    break;
+                case "*" : 
+                    S.push(S.pop() * S.pop()); 
+                    break;
+                case "/" : 
+                    int n2 = S.pop(), n1 = S.pop();
+                    S.push(n1 / n2); 
+                    break;
+                default :
+                    S.push(Integer.parseInt(token));
+           }
         }
-        return s.pop();
-    }
-    private void calculate(String token, Stack<Integer> s) {
-        int n2 = s.pop();
-        int n1 = s.pop();
-        if (token.equals("/")) s.push(n1 / n2);
-        else if (token.equals("*")) s.push(n1 * n2);
-        else if (token.equals("+")) s.push(n1 + n2);
-        else s.push(n1 - n2);
+        return S.pop();
     }
 }
