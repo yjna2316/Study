35bebe6b7e51d64f5c75723b3ce5869716f31873
Yoonji Na <yjna2316@naver.com>
2017-07-13 13:54:51 +0900

    ascii, char as index of an array

--- DataStructureAlgorithm/String and Array/ValidAnagram.java
+++ DataStructureAlgorithm/String and Array/ValidAnagram.java
@@ -3,16 +3,20 @@
  * https://leetcode.com/problems/valid-anagram/#/description
  */
 
+/* The idea is to create a size 26 int arrays as buckets for each letter in alphabet. 
+ * It increments the bucket value with String s and decrement with string t. 
+ * So if they are anagrams, all buckets should remain with initial value which is zero. 
+ */
 public class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) return false;
-        int[] counts = new int[26];
+        int[] alphabet = new int[26];
         for (int i = 0; i < s.length(); i ++) {
-            counts[s.charAt(i) - 'a'] ++;
-            counts[t.charAt(i) - 'a'] --;
+            alphabet[s.charAt(i) - 'a'] ++;
+            alphabet[t.charAt(i) - 'a'] --;
         }
-        for (int count : counts) {
-            if(count != 0) return false;
+        for (int val : alphabet) {
+            if(val != 0) return false;
         }
         return true;
     }
