06198a066c37f82986145babeefb143eda5c5ce1
Yoonji Na <yjna2316@naver.com>
2017-07-13 13:54:14 +0900

    two pointers, sorted array

--- DataStructureAlgorithm/String and Array/TwoSum2.java
+++ DataStructureAlgorithm/String and Array/TwoSum2.java
@@ -1,15 +1,14 @@
 /* 
- * 167. Two Sum II - Input array is sorted
+ * 167. Two Sum II 
  * https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/#/description
  */
 
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int L = 0, R = numbers.length - 1;
-        while (L < R) {
-            if (numbers[L] + numbers[R] < target) L ++;
-            else if (numbers[L] + numbers[R] > target) R--;
-            else break;
+        while (numbers[L] + numbers[R] != target) {
+            if (numbers[L] + numbers[R] < target) L ++; 
+            else R --;
         }
         return new int[]{L + 1, R + 1};
     }
