811c212b8afbbadfdd1321c8dd78f7bfae2edb59
Yoonji Na <yjna2316@naver.com>
2017-07-19 17:07:47 +0900

    hash map, two pointers, count array, ascii

--- DataStructureAlgorithm/String and Array/LongestSubstringWithoutRepeatingCharacters.java
+++ DataStructureAlgorithm/String and Array/LongestSubstringWithoutRepeatingCharacters.java
@@ -19,3 +19,23 @@ public class Solution {
         return (max > len) ? max : len; 
     }
 }
+
+/* 
+ * Count array: use an array to count the occurrence of characters of a string,
+ *              the indexes will be "characters". 
+ */
+ public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        boolean[] exist = new boolean[128];//왜256?
+        int i = 0, maxLen = 0;
+        for (int j = 0; j < s.length(); j ++) {
+            while (exist[s.charAt(j)]) {
+                exist[s.charAt(i)] = false;
+                i ++;
+            }
+            exist[s.charAt(j)] = true;
+            maxLen = Math.max(j - i + 1, maxLen);
+        }
+        return maxLen;
+    }
+}
