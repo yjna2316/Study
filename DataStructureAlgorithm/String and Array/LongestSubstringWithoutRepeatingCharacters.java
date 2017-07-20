9311282e7089d83d4f9bf2bb99b65b49d70ed4f3
Yoonji Na <yjna2316@naver.com>
2017-07-20 16:41:14 +0900

    hashMap, sliding window [i,j], two pointers

--- DataStructureAlgorithm/String and Array/LongestSubstringWithoutRepeatingCharacters.java
+++ DataStructureAlgorithm/String and Array/LongestSubstringWithoutRepeatingCharacters.java
@@ -3,30 +3,43 @@
  * https://leetcode.com/problems/longest-substring-without-repeating-characters/#/discuss
  */
 
+/* Solution 1 - Sliding Window using int[128] for ASCII 128 */
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        int len = 0, max = 0;
-        for (int i = 0; i < s.length(); i ++) {
-            char c = s.charAt(i);
-            if(map.containsKey(c) && len >= i - map.get(c)) {  // if duplicates
-                max = (max > len) ? max : len;  // max update
-                len = i - map.get(c) - 1;  // new length
-            }
-            map.put(c, i);
-            len ++;            
+        if (s.length() <= 1) return s.length();
+        int[] index = new int[128];
+        int max = 0;
+        for (int i = 0, j = 0; j < s.length(); j ++) {
+            i = Math.max(index[s.charAt(j)], i);
+            max = Math.max(max, j - i + 1);
+            index[s.charAt(j)] = j + 1;
         }
-        return (max > len) ? max : len; 
+        return max;
     }
 }
 
-/* 
- * Count array: use an array to count the occurrence of characters of a string,
- *              the indexes will be "characters". 
- */
+/* Solution 2 - Siding Window using HashMap */
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() <= 1) return s.length();
+        Map<Character, Integer> map = new HashMap<>();
+        int max = 0;
+        for (int i = 0, j = 0; i < s.length(); i ++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {  // if duplicates
+                j = Math.max(j , map.get(c) + 1); 
+            }
+            map.put(c, i);
+            max = Math.max(max , i - j + 1);
+        }
+        return max; 
+    }
+}
+
+/* Solution 3 - boolean[256] for Extended ASCII */
  public class Solution {
     public int lengthOfLongestSubstring(String s) {
-        boolean[] exist = new boolean[128];//왜256?
+        boolean[] exist = new boolean[256];
         int i = 0, maxLen = 0;
         for (int j = 0; j < s.length(); j ++) {
             while (exist[s.charAt(j)]) {
