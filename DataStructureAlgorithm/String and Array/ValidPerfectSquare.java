c386280e9ff14d4fb891b3628e92fb90fbad8da2
yjna2316 <yjna2316@naver.com>
2017-07-27 14:57:01 +0900

    Update ValidPerfectSquare.java

--- DataStructureAlgorithm/String and Array/ValidPerfectSquare.java
+++ DataStructureAlgorithm/String and Array/ValidPerfectSquare.java
@@ -3,6 +3,7 @@
  * 367. Valid Perfect Square
  */
 
+/* solution 1 - binary search */
 public class Solution {
     public boolean isPerfectSquare(int num) {
         int left = 1, right = num;
@@ -19,6 +20,8 @@ public class Solution {
         return false;
     }
 }
+
+/* solution 2 - without binary search */
 public class Solution {
     public boolean isPerfectSquare(int num) {
         for (int x = 1; x <= num / 2; x ++) 
