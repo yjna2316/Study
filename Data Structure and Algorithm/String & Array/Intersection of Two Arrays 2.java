ada8a34628733f9131584fe310e9fde5bdc7c490
Yoonji Na <yjna2316@naver.com>
2017-09-25 16:27:10 +0900

    hashmap, two pointers

--- Data Structure and Algorithm/String & Array/Intersection of Two Arrays 2.java
+++ Data Structure and Algorithm/String & Array/Intersection of Two Arrays 2.java
@@ -1,45 +1,92 @@
 /**
- * 350. Intersection of Two Arrays 2
- * https://leetcode.com/problems/intersection-of-two-arrays-ii/discuss/
- * Next: (E)Intersection of Two Arrays
+ * 
+ *
+ *
  */
- 
+
+/* Solution 1: HashMap */ 
 import java.util.*;
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         
-        int len1 = nums1.length;
-        int len2 = nums2.length;
-        if (len1 * len2 == 0)
+        // If the one of the size of the arrays is zero, return an empty array.
+        if (nums1.length * nums2.length == 0)
         {
             return new int[]{};
-        }   
-
-        int len = (len1 > len2) ? len1 : len2;
-        ArrayList<Integer> result = new ArrayList<>();
-        boolean[] checked = new boolean[len];
-
-        for (int i = 0; i < len1; i ++)
+        }
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int val : nums1)
         {
-            for (int j = 0; j < len2; j ++)
-            {
-                if (!checked[j] && nums1[i] == nums2[j])
-                {
-                    System.out.println(nums1[i] + " " + nums2[j]);
-                    checked[j] = true;
-                    result.add(nums1[i]);
-                    break;
-                }
+            if (map.containsKey(val)) {
+                map.put(val, map.get(val) + 1);
+            } else {
+                map.put(val, 1);
             }
         }
         
-        // arraylist to array
-        int[] r = new int[result.size()];
-        
-        for (int i = 0; i < result.size(); i ++)
+        int len = (nums1.length > nums2.length) ? nums1.length : nums2.length;
+        ArrayList<Integer> a = new ArrayList<>();
+       
+        for (int val : nums2)
         {
-            r[i] = result.get(i);
+            if (map.containsKey(val))
+            {
+                if(map.get(val) > 0)
+                {
+                    map.put(val, map.get(val) - 1);
+                    a.add(val);
+                }
+            }
+        }
+        // ArrayList to array
+        int[] r = new int[a.size()];
+        
+        for (int i = 0; i < a.size(); i ++)
+        {
+            r[i] = a.get(i);
         }
         return r;
+    }
+        
+}
+
+/* Solution2: two pointers */
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        
+        if (nums1.length * nums2.length == 0)
+        {
+            return new int[]{};
+        }
+        
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        
+        ArrayList<Integer> a = new ArrayList<>();
+       
+        int i = 0, j = 0;
+        while (i < nums1.length  && j < nums2.length){
+            if (nums1[i] < nums2[j]) {
+               i ++;
+            } 
+            else if (nums1[i] > nums2[j]) {
+                j ++;
+            } 
+            else {
+                a.add(nums1[i]);
+                i ++;
+                j ++;
+            }
+        }
+        
+        int[] result = new int[a.size()];
+        
+        for (int k = 0; k < a.size(); k ++)
+        {
+            result[k] = a.get(k);
+        }
+        
+        return result;    
     }
 }
