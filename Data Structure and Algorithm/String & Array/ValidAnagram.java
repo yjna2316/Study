f25bbba4b8443a1e081af3830650e3542d1ba69a
Yoonji Na <yjna2316@naver.com>
2017-09-27 05:17:26 +0900

    hashtable, sort

--- Data Structure and Algorithm/String & Array/ValidAnagram.java
+++ Data Structure and Algorithm/String & Array/ValidAnagram.java
@@ -1,24 +1,28 @@
 /* 
  * 242. Valid Anagram
  * https://leetcode.com/problems/valid-anagram/#/description
+ * Next: (M)Group Anagrams (E)Palindrome Permutation (E)Find All Anagrams in a String 
  */
 
-/* 
- * The idea is to create a size 26 int arrays as buckets for each letter in alphabet. 
- * It increments the bucket value with String s and decrement with string t. 
- * So if they are anagrams, all buckets should remain with initial value which is zero. 
- */
 public class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
+        if (s.length() != t.length()) 
+        {
+            return false;
+        }
+
         int[] alphabet = new int[26];
-        for (int i = 0; i < s.length(); i ++) {
+        for (int i = 0; i < s.length(); i ++) 
+        {
             alphabet[s.charAt(i) - 'a'] ++;
             alphabet[t.charAt(i) - 'a'] --;
         }
-        for (int val : alphabet) {
+
+        for (int val : alphabet) 
+        {
             if(val != 0) return false;
         }
+
         return true;
     }
 }
