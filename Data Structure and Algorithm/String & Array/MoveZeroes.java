6b559a3bcab9e7bc4abb9dd8ab8353d3643482f4
Yoonji Na <yjna2316@naver.com>
2017-09-27 04:34:44 +0900

    two pointers, array

--- Data Structure and Algorithm/String & Array/MoveZeroes.java
+++ Data Structure and Algorithm/String & Array/MoveZeroes.java
@@ -1,18 +1,44 @@
 /*
- * 283. Move Zeroes
- * https://leetcode.com/problems/move-zeroes/#/discuss
+ * 283. Move Zeroem0s
+ * https://leetco9de.com/problems/move-zeroes/#/discuss
+ * Next: (E)Remove Element (M)3Sum (M)Subsets (M)Split Array with Equal Sum
  */
 
+/* Solution 1 */
 public class Solution {
     public void moveZeroes(int[] nums) {
         int insertPos = 0; 
         /* shift non-zero elements to the front of array keeping their relative order */
-        for (int num : nums) {
-            if (num != 0) nums[insertPos ++] = x;
+        for (int val : nums) 
+        {
+            if (val != 0) 
+            {
+                nums[insertPos ++] = val;
+            }
         }
         /* fill remaining space with zeroes */
-        while (insertPos < nums.length) {
+        while (insertPos < nums.length) 
+        {
             nums[insertPos ++] = 0;
+        }
+    }
+}
+
+/* Solution 2 - swap two positions while iterating*/
+class Solution {
+    public void moveZeroes(int[] nums) {
+        
+        int insertPos = 0;
+        
+        for (int i = 0; i < nums.length; i ++)
+        {
+            if (nums[i] != 0)
+            {
+                int temp = nums[insertPos];
+                nums[insertPos] = nums[i];
+                nums[i] = temp;
+                insertPos ++;
+            }
         }
     }
 }
